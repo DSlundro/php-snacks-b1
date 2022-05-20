76f0a3127532b2be83608239f7ad900fe411ac02
DSlundro <dragan.savic95@outlook.it>
2022-05-20 12:43:23 +0200

    initial commit

---
+++ snack-1/index.php
@@ -0,0 +1,49 @@
+<?php
+## Snack 1
+// Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+// Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+// Stampiamo a schermo tutte le partite con questo schema.
+// Olimpia Milano - Cantù | 55-60
+
+
+// creo un array con le partite
+
+$arrayMatch = [
+    [
+        "id" => "1",
+        "match" => "20/05/2022",
+        "homeTeam" => "Olimpia Milano",
+        "awayTeam" => "Cantù",
+        "homePoints" => "55",
+        "awayPoints" => "60",
+    ],
+    [
+        "id" => "2",
+        "match" => "22/05/2022",
+        "homeTeam" => "Torino",
+        "awayTeam" => "Venezia",
+        "homePoints" => "75",
+        "awayPoints" => "44",
+    ],
+];
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    
+
+
+
+</body>
+</html>
